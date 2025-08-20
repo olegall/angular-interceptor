63f4f96fc5ef326f0f2d6c94024e9816ed12842e
oleg aleynikov <aleynikov@raduga-lik.ru>
2025-08-20 09:29:30 +0300

    removed unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
-import { importProvidersFrom, NgModule, Provider } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
